d8ccf726f28999fe5fd4512d1480a6b7f4f116ec
TanyaSt <TanyaSt@users.noreply.github.com>
2022-12-13 10:07:27 +0200

    check

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, Fragment} from "react";
+import "./App.css";
+import data from "./services/api.js";
+import ReadOnlyRow from "./components/ReadOnlyRow";
+import EditableRow from "./components/EditableRow";
+import {toHaveFormValues} from "@testing-library/jest-dom/dist/matchers";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const App = () => {
+  const [contacts, setContacts] = useState(data);
+  const [editContactId, setEditContactId] = useState(null);
+
+  const handleEditClick = (event, contact)=> {
+    event.preventDefault();
+    setEditContactId(contact.id);
+  }
+  const handleEditFormSubmit =(event)=> {
+    event.preventDefault();
+    const editContact ={
+      id: editFormData.id,
+      firstName: editFormData.firstName,
+      lastName: editFormData.lastName,
+      email: editFormData.email,
+      phone: editFormData.phone,
+      gender: editFormData.gender,
+    };
+         setEditFormData(toHaveFormValues);
+  };
+
+  const handleDeleteClick = (contactId) =>{
+      const newContacts = [...contact];
+
+      const index = contacts.findIndex((contact)=> contact.id === contactId);
+      newContacts.splice(index, 1);
+      setContacts(newContacts);
+  }
+
+  return( <div className="app=container">
+        <form>
+    <table>
+      <thead>
+        <tr>
+          <th>SN</th>
+          <th>First Name</th>
+          <th>Last Name</th>
+          <th>Email</th>
+          <th>Phone</th>
+          <th>Gender</th>
+          <th>Update</th>
+          <th>Delete</th>
+
+        </tr>
+      </thead>
+      <tbody>
+        {contacts.map((contact)=> (
+            <Fragment>
+              {editContactId === contact.id ? (<EditableRow />
+              ) : (
+              <ReadOnlyRow contact ={contact}
+                           handleEditClick={handleEditClick}
+                           handleDeleteClick={handleDeleteClick}
+              />
+              )}
+            </Fragment>
+              ))}
+            </tbody>
+          </table>
+        </form>
+        );
+  </div>
   );
-}
+};
 
 export default App;
+
